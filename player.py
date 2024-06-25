c10fb593051aee0cc1bbe8e08d4ecc4b90c4707d
Vardhin <115967495+vardhin@users.noreply.github.com>
2024-06-25 18:28:11 +0530

    Update player.py

--- player.py
+++ player.py
@@ -1,4 +1,5 @@
 import pygame
+from settings import *
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, sprite_sheet_path, pos, size, frame_rate=5):
@@ -34,7 +35,7 @@ class Player(pygame.sprite.Sprite):
             frames.append(row_frames)
         return frames
 
-    def handle_movement(self, keys, obstacles):
+    def handle_movement(self, keys, obstacles, camera_x, camera_y, zoom):
         speed = 5
         self.is_moving = False
         new_pos = self.pos[:]
@@ -65,6 +66,18 @@ class Player(pygame.sprite.Sprite):
         self.update_rects()
         self.update_image()
 
+        # Update camera position
+        if self.rect.x < camera_x + CAMERA_BUFFER:
+            camera_x = self.rect.x - CAMERA_BUFFER
+        elif self.rect.x + self.rect.width > camera_x + WIDTH / zoom - CAMERA_BUFFER:
+            camera_x = self.rect.x + self.rect.width - WIDTH / zoom + CAMERA_BUFFER
+        if self.rect.y < camera_y + CAMERA_BUFFER:
+            camera_y = self.rect.y - CAMERA_BUFFER
+        elif self.rect.y + self.rect.height > camera_y + HEIGHT / zoom - CAMERA_BUFFER:
+            camera_y = self.rect.y + self.rect.height - HEIGHT / zoom + CAMERA_BUFFER
+
+        return camera_x, camera_y
+
     def update_rects(self):
         self.rect.topleft = self.pos
         self.collision_rect.topleft = (self.pos[0], self.pos[1] + self.size[1] - 10)
@@ -80,71 +93,3 @@ class Player(pygame.sprite.Sprite):
         scaled_image = pygame.transform.scale(self.image, (int(self.size[0] * zoom), int(self.size[1] * zoom)))
         screen.blit(scaled_image, (int((self.rect.x - camera_x) * zoom), int((self.rect.y - camera_y) * zoom)))
         return pygame.mask.from_surface(scaled_image)
-
-class Obstacle(pygame.sprite.Sprite):
-    def __init__(self, pos, size):
-        super().__init__()
-        self.image = pygame.Surface(size, pygame.SRCALPHA)
-        self.rect = self.image.get_rect(topleft=pos)
-        self.true_collide_rect = pygame.Rect(pos[0], pos[1], size[0], size[1])
-
-if __name__ == '__main__':
-    pygame.init()
-
-    SCREEN_WIDTH = 800
-    SCREEN_HEIGHT = 600
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    pygame.display.set_caption("Masks")
-
-    BG = (0, 0, 0)
-    RED = (255, 0, 0)
-    WHITE = (255, 255, 255)
-    zoom = 1.0
-
-    player = Player(sprite_sheet_path='assets/player.png', pos=[SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2], size=[64, 64])
-    obstacles = pygame.sprite.Group(Obstacle((100, 100), (50, 50)), Obstacle((300, 300), (50, 50)))
-
-    all_sprites = pygame.sprite.Group(player, *obstacles)
-    camera_x, camera_y = player.rect.x - SCREEN_WIDTH // 2, player.rect.y - SCREEN_HEIGHT // 2
-
-    running = True
-    clock = pygame.time.Clock()
-
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-
-        keys = pygame.key.get_pressed()
-        player.handle_movement(keys, obstacles)
-
-        screen.fill(BG)
-
-        # Draw the player
-        player_mask = player.draw(screen, camera_x, camera_y, zoom)
-        
-        # Draw the obstacles
-        for obstacle in obstacles:
-            obstacle_rect_scaled = pygame.Rect(
-                (obstacle.rect.x - camera_x) * zoom,
-                (obstacle.rect.y - camera_y) * zoom,
-                obstacle.rect.width * zoom,
-                obstacle.rect.height * zoom
-            )
-            screen.blit(obstacle.image, obstacle_rect_scaled.topleft)
-
-        bullet = pygame.Surface((10, 10))
-        bullet.fill(RED)
-        bullet_rect = bullet.get_rect(topleft=pygame.mouse.get_pos())
-        screen.blit(bullet, bullet_rect.topleft)
-
-        bullet_mask = pygame.mask.from_surface(bullet)
-        offset = (bullet_rect.left - player.rect.left, bullet_rect.top - player.rect.top)
-        
-        if player_mask.overlap(bullet_mask, offset):
-            print("collided")
-
-        pygame.display.flip()
-        clock.tick(60)
-
-    pygame.quit()
